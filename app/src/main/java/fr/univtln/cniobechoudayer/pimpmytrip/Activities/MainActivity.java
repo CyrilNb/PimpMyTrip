42f91a45e4e4fd1f9a04c84109e5b4dbea20d0e6
Cyril <cyril.niobe@gmail.com>
2017-11-21 20:34:06 +0100

    init dev branch with Activities and Utils packages

--- app/src/main/java/fr/univtln/cniobechoudayer/pimpmytrip/Activities/MainActivity.java
+++ app/src/main/java/fr/univtln/cniobechoudayer/pimpmytrip/Activities/MainActivity.java
@@ -1,7 +1,9 @@
-package fr.univtln.cniobechoudayer.pimpmytrip;
+package fr.univtln.cniobechoudayer.pimpmytrip.Activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import fr.univtln.cniobechoudayer.pimpmytrip.R;
 
 public class MainActivity extends AppCompatActivity {
 
