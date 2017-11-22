db66f8e4edaa7f299bc0c3704ce56562f5af6046
Cyril <cyril.niobe@gmail.com>
2017-11-22 01:43:11 +0100

    ADD SignUpActivity

--- app/src/main/java/fr/univtln/cniobechoudayer/pimpmytrip/Authentication.java
+++
@@ -1,8 +0,0 @@
-package fr.univtln.cniobechoudayer.pimpmytrip;
-
-/**
- * Created by Cyril Niobé on 21/11/2017.
- */
-
-public class Authentication {
-}
---
+++ app/src/main/java/fr/univtln/cniobechoudayer/pimpmytrip/Authentication/SignUpActivity.java
@@ -0,0 +1,9 @@
+package fr.univtln.cniobechoudayer.pimpmytrip.Authentication;
+
+/**
+ * SignUpAcitivity using Firebase auth
+ * Created by Cyril Niobé on 22/11/2017.
+ */
+
+public class SignUpActivity {
+}
